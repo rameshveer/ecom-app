ff51e74d7a7f0e48dd2118b5dc6300311a59e563
Ramesh Veer <rameshveerfeb27@gmail.com>
2019-09-11 18:04:02 +0530

    v3

--- item_api/serializers.py
+++ item_api/serializers.py
@@ -1,6 +1,16 @@
 from rest_framework import serializers
 
 
-class HelloSerializer(serializers.Serializer):
-    """Serializes a name field for testing our APIView"""
-    name = serializers.CharField(max_length=10)
+class Item_Serializer(serializers.Serializer):
+    """Serializes a fields our APIView"""
+    item_name   = serializers.CharField(max_length=10)
+    item_no     = serializers.CharField(max_length=10)
+
+class Item_Post_Serializer(serializers.Serializer):
+    """Serializes a fields our APIView"""
+    item_name   = serializers.CharField(max_length=10)
+    item_no     = serializers.CharField(max_length=10)
+    item_desc   = serializers.CharField(max_length=50)
+    item_qty    = serializers.IntegerField(max_value=10000, min_value=1)
+    item_brand  = serializers.CharField(max_length=20)
+    item_price  = serializers.DecimalField(max_digits=9, decimal_places=2)
--- item_api/views.py
+++ item_api/views.py
@@ -4,37 +4,51 @@ from rest_framework import status
 
 from item_api import serializers
 
+from Item_Management import  Item_Master, Item_Controller
 from Item_Management_Srch import Srch_Item_Param, Item_Search
 
 
 class ItemApiView(APIView):
     """ Test API View"""
-    serializer_class = serializers.HelloSerializer
+    item_get = serializers.Item_Serializer
+    item_post = serializers.Item_Post_Serializer
 
     def get(self, request, format=None):
-        """" Returns a list of APIView features"""
-        an_apiview = [
-            'Uses HTTP methods as function (get, post, patch, put, delete)',
-            'Is similar to a traditional Django view',
-            'Gives you the most control over your application logic',
-            'Is mapped manually to URLs',
-        ]
+        item_get_ser = self.item_get(data=request.data)
 
-        return Response({'message':'Hello!', 'an_apiview': an_apiview})
+        if item_get_ser.is_valid():
+            item_name = item_get_ser.validated_data.get('item_name')
+            item_no = item_get_ser.validated_data.get('item_no')
 
-    def post(self, request):
-        """Create a hello message with our name"""
-        serializer = self.serializer_class(data=request.data)
+            #item_nm = f'{item_name}'
 
-        if serializer.is_valid():
-            name = serializer.validated_data.get('name')
-            item = f'{name}'
-            srch1 = Srch_Item_Param(Item_Name =item, Item_No ='3', Item_Brand='')
+            srch1 = Srch_Item_Param(Item_Name =item_name, Item_No =item_no, Item_Brand='')
             srch_result = Item_Search(srch1)
 
-            return Response({'message': item, 'data':srch_result})
+            return Response({'Item Name': item_name, 'data':srch_result})
         else:
-            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+            return Response(item_get_ser.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def post(self, request):
+        item_post_ser = self.item_post(data=request.data)
+
+        if item_post_ser.is_valid():
+            item_name = item_post_ser.validated_data.get('item_name')
+            item_no = item_post_ser.validated_data.get('item_no')
+            item_desc = item_post_ser.validated_data.get('item_desc')
+            item_qty = item_post_ser.validated_data.get('item_qty')
+            item_brand = item_post_ser.validated_data.get('item_brand')
+            item_price = item_post_ser.validated_data.get('item_price')
+
+            item_insert_data = Item_Master(Ino =item_no, Iname=item_name, Idesc=item_desc, Iqty=item_qty, Ibrand=item_brand, Iprice=item_price)
+
+            item_insert = Item_Controller()
+
+            item_insert.Save_ItemMaster(item_insert_data)
+
+            return Response({'Item Insert': 'inserted into ITEM_MASTER Successfully'})
+        else:
+            return Response(item_post_ser.errors, status=status.HTTP_400_BAD_REQUEST)
 
     def put(self, request, pk=None):
        """"Handle updating an object"""
